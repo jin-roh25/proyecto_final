6186b8fa34a210217b929c51554c09c10fb178c3
jin-roh25 <sebag2511@gmail.com>
2022-11-22 11:44:39 -0300

    class principales

---
+++ ProyectoFinal/src/proyectofinal/Ventana.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectofinal;
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ *
+ * @author segonzalez2021
+ */
+public class Ventana extends JFrame {
+    
+    private PanelPrincipal p;
+    
+    public Ventana(){
+        this.setLayout(new BorderLayout());
+        this.setTitle("Deposito de Bebidas");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(515, 538); //contrarresta el achicamiento
+        this.setResizable(false);
+
+        p = new PanelPrincipal();
+        p.setSize(500, 500);
+        this.add(p, BorderLayout.CENTER);
+
+        this.setVisible(true);
+    }
+}
