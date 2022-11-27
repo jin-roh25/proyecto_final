01b95c61dcfe07d072bba9b092d9b2191becbc91
Keyteer <>
2022-11-27 15:38:21 -0300

    bola momentum transfer

--- ProyectoFinal/src/proyectofinal/Bola.java
+++ ProyectoFinal/src/proyectofinal/Bola.java
@@ -1,52 +1,85 @@
 package proyectofinal;
 
 import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
 
-public class Bola extends javax.swing.JPanel {
+public class Bola {
 
-    private int numero;
-    private int dx, dy;
-    private Timer time;
+    private final int numero;
+    private double x, dx;
+    private double y, dy;
+    private final int tamano;
 
-    public Bola() {
-        numero = 0;
-        dx = 0;
-        dy = 0;
-        initComponents();
-
-        time = new Timer(10, new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                movimiento();
-            }
-        });
-        time.start();
+    public Bola(double x, double y, int numero) {
+        this.numero = numero;
+        this.x = x;
+        this.y = y;
+        this.dx = 0;
+        this.dy = 0;
+        this.tamano = 30;
     }
 
-    private void movimiento() {
-        this.setLocation(this.getX() + dx, this.getY() + dy);
+    public void setDelta(double dx, double dy) {
+        this.dx = dx;
+        this.dy = dy;
     }
 
-    @Override
+    public void addDelta(double dx, double dy) {
+        this.dx += dx;
+        this.dy += dy;
+    }
+
+    public double getDX() {
+        return dx;
+    }
+
+    public double getDY() {
+        return dy;
+    }
+
+    public void setLocation(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public int getTamaño() {
+        return tamano;
+    }
+
+    public void movimiento() {
+        x += dx;
+        y += dy;
+    }
+
+    public void momentunTransferToBola(Bola b) {
+        double implsX;
+        double implsY;
+
+        double proporcion = (Math.PI / 2 - Math.abs(Math.atan2(
+                b.getY() - y, b.getX() - x) - Math.atan2(dy, dx)))
+                / (Math.PI / 2);
+
+        if (proporcion < 0) {
+            proporcion = 0;
+        }
+
+        implsX = Math.cos(Math.atan2(b.getY() - y, b.getX() - x)) * proporcion * Math.sqrt(dx * dx + dy * dy);
+        implsY = Math.sin(Math.atan2(b.getY() - y, b.getX() - x)) * proporcion * Math.sqrt(dx * dx + dy * dy);
+
+        b.addDelta(implsX, implsY);
+        this.addDelta(-implsX, -implsY);
+    }
+
     public void paint(Graphics g) {
-        super.paint(g);
         g.setColor(new Color(0, 0, 0));
-        g.fillOval(0, 0, this.getWidth(), this.getHeight());
+        g.fillOval((int) x - tamano / 2, (int) y - tamano / 2,
+                tamano, tamano);
     }
-
-    @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
-
-        setOpaque(false);
-        setPreferredSize(new java.awt.Dimension(40, 40));
-        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-    }// </editor-fold>//GEN-END:initComponents
-
-
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    // End of variables declaration//GEN-END:variables
 }
